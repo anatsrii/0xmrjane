169aece2f0dfdd2911fdc904c09100596d06382b
anatsrii <anat.allsolution@gmail.com>
2022-08-12 15:21:50 +0700

    Home component responsive done

--- front/src/Components/Home.js
+++ front/src/Components/Home.js
@@ -1,40 +1,60 @@
-import { Button, Grid } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
-import { Link } from 'react-router-dom';
-import '../Style/Home.css';
-
-
-
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import { Link } from "react-router-dom";
+import "../Style/Home.css";
+import { Box } from "@mui/material";
 
 function Home() {
-  
   return (
     // Main container
-    <Grid container justifyContent='center' alignItems='center' className='container'> 
+    <Container 
+    >
+      <Box component="div" className="container">
 
-      {/* Left box */}
-      <Grid className='box1' >
+        {/* Left box */}
+        <Box  className="box1" >
+          {/* Image */}
+          <Avatar alt="profile" src="profilecorp.png" className="avarta" />
+        </Box>
 
-        {/* Image */}
-      <Avatar 
-        alt="profile"
-        src="profilecorp.png"
-        className='avarta'
-      />
-      </Grid>
+        {/* Right Box Menu */}
+        <Box  className="box2">
+          <h1>0xMrJane</h1>
+          <p>ANAT SRISAPUTH</p>
+          <p>
+            <Button className="button" component={Link} to="portfolio">
+              PORTFOLIO
+            </Button>
+          </p>
+          <p>
+            <Button className="button" component={Link} to="projects">
+              PROJECTS
+            </Button>
+          </p>
+          <p>
+            <Button
+              className="button"
+              onClick={() => window.open("https://github.com/anatsrii")}
+              >
+              GITHUB
+            </Button>
+          </p>
+          <p>
+            <Button className="button" component={Link} to="contact">
+              CONTACT ME
+            </Button>
+          </p>
+          <p>
+            <Button className="button" component={Link} to="blog">
+              MY BLOG
+            </Button>
+          </p>
 
-      {/* Right Box Menu */}
-      <Grid row >
-        <h1>0xMrJane</h1>
-        <p>ANAT SRISAPUTH</p>
-        <p><Button className='button' component={Link} to='portfolio'>PORTFOLIO</Button></p>
-        <p><Button className='button' component={Link} to='projects'>PROJECTS</Button></p>
-        <p><Button className='button' onClick={()=> window.open('https://github.com/anatsrii')}>GITHUB</Button></p>
-        <p><Button className='button' component={Link} to='contact'>CONTACT ME</Button></p>
-        <p><Button className='button' component={Link} to='blog'>MY BLOG</Button></p>
-      </Grid>
-    </Grid>
-  )
+      </Box>
+    </Box>
+    </Container>
+  );
 }
 
 export default Home;
